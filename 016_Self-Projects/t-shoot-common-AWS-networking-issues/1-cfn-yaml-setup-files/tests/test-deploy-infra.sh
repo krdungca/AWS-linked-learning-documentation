f26063b48b8ba157f28e744c598badc6aad5fb91
Kenneth Dungca <ken.rodulfo.dungca@gmail.com>
2026-03-18 03:19:58 +0000

    added test-deploy-infa.sh, auto-rename stack name with TEST-VERSION Number, added tag Limit:Demo

--- 016_Self-Projects/t-shoot-common-AWS-networking-issues/1-cfn-yaml-setup-files/tests/test-deploy-infra.sh
+++ 016_Self-Projects/t-shoot-common-AWS-networking-issues/1-cfn-yaml-setup-files/tests/test-deploy-infra.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-STACK_NAME="ManilaStoreLab"
+# --- New Input Section ---
+echo -n "Enter the test version number: "
+read VERSION
+
+# Dynamically update names based on input
+STACK_NAME="ManilaStoreLab-TestVersion-$VERSION"
+TEMPLATE_FILE="test-cfn-template-v$VERSION.yaml"
+# -------------------------
+
 REGION="us-east-1"
-TEMPLATE_FILE="manilastorelab-infra.yaml"
 JSON_FILE="infra-ids.json"
 
-echo "🚀 Deploying Manila-Store Infrastructure..."
+echo "🚀 Deploying $STACK_NAME using $TEMPLATE_FILE..."
 
 # 1. Start Stack Creation
 aws cloudformation create-stack \
@@ -19,7 +26,7 @@ aws cloudformation wait stack-create-complete --stack-name $STACK_NAME --region 
 
 if [ $? -eq 0 ]; then
     echo "✅ Success! Generating $JSON_FILE..."
-    
+
     # 3. Pull all outputs and format them into a clean JSON object
     aws cloudformation describe-stacks \
     --stack-name $STACK_NAME \
